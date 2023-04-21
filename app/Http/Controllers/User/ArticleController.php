72d29ac7a3be3f0ddf912b3dbe7ed24dcb2cc943
Hafiizh Ghulam <hafiizh.ghulam@frisidea.com>
2023-04-22 00:55:14 +0700

    Article for User Side

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -7,8 +7,15 @@ use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
 use App\Models\ArticleModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ArticleController extends Controller {
+    protected $articleTable;
+
+    public function __construct() {
+        $this->articleTable = (new ArticleModel())->getTable();
+    }
+
     public function getHome(Request $request) {
         $articles = ArticleModel::where("type", ArticleTypeConstant::HOME)->orderByDesc("id")->get();
 
@@ -19,5 +26,18 @@ class ArticleController extends Controller {
         $articles = ArticleModel::where("type", ArticleTypeConstant::TOUR)->orderByDesc("id")->paginate();
 
         return ResponseHelper::response($articles);
+    }
+
+    public function getDetail(Request $request, $id) {
+        $validator = Validator::make([
+            "id" => $id
+        ], [
+            "id" => "required|numeric|exists:$this->articleTable,id"
+        ]);
+        if ($validator->fails()) return ResponseHelper::response(null, $validator->errors()->first(), 400);
+
+        $article = ArticleModel::find($id);
+
+        return ResponseHelper::response($article);
     }
 }
--- routes/user/article.php
+++ routes/user/article.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("get/home", "ArticleController@getHome");
 Route::get("get/tour", "ArticleController@getTour");
+Route::get("get/detail/{id}", "ArticleController@getDetail");
