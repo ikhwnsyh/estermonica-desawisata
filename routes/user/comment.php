98c34232990d9397723cb5df76a57034d20df3c2
Hafiizh Ghulam <hafiizh.ghulam@frisidea.com>
2023-04-22 09:35:03 +0700

    adjustment

--- routes/user/comment.php
+++ routes/user/comment.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Constants\TokenConstant;
 use Illuminate\Support\Facades\Route;
 
 Route::get("get", "CommentController@get");
-Route::post("add", "CommentController@add");
+Route::middleware([TokenConstant::AUTH_SANCTUM, TokenConstant::AUTH_USER])->post("add", "CommentController@add");
