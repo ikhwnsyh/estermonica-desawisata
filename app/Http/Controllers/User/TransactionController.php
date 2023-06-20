67cf7ed792e5197e93cda2953cb92b19df716a55
Hafiizh Ghulam <hafiizh.ghulam@frisidea.com>
2023-06-20 22:12:52 +0700

    adjustments

--- app/Http/Controllers/User/TransactionController.php
+++ app/Http/Controllers/User/TransactionController.php
@@ -8,13 +8,48 @@ use App\Http\Controllers\Controller;
 use App\Models\TransactionHistoryModel;
 use App\Models\TransactionModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class TransactionController extends Controller {
-    protected $transactionTable;
+    protected $transactionTable, $transactionHistoryTable;
 
     public function __construct() {
         $this->transactionTable = (new TransactionModel())->getTable();
+        $this->transactionHistoryTable = (new TransactionHistoryModel())->getTable();
+    }
+
+    public function getData(array $conditions = [], $id = null) {
+        $detailIds = DB::table("$this->transactionHistoryTable as detail_mx")
+            ->selectRaw("max(detail_mx.id) as detail_id, detail_mx.transaction_id")
+            ->groupBy("detail_mx.transaction_id")
+            ->toSql();
+        $detailData = DB::table("$this->transactionHistoryTable as detail_data")
+            ->selectRaw("detail_data.id, detail_data.status")
+            ->toSql();
+        $data = TransactionModel::with("latestHistory", "histories", "ticketBundle.tickets", "user")
+            ->select("$this->transactionTable.*")
+            ->leftJoinSub(
+                $detailIds,
+                "detail_max",
+                "$this->transactionTable.id",
+                "=",
+                "detail_max.transaction_id"
+            )
+            ->leftJoinSub(
+                $detailData,
+                "detail",
+                "detail.id",
+                "=",
+                "detail_max.detail_id"
+            )
+            ->whereRaw(implode(" and ", $conditions))
+            ->orderByDesc("$this->transactionTable.id");
+
+        if (empty($id)) $data = $data->paginate();
+        else $data = $data->find($id);
+
+        return $data;
     }
 
     public function get(Request $request) {
@@ -23,6 +58,12 @@ class TransactionController extends Controller {
             ->paginate();
 
         return ResponseHelper::response($data);
+    }
+
+    public function active(Request $request) {
+        return ResponseHelper::response($this->getData([
+            "detail.status = " . MidtransStatusConstant::SETTLEMENT
+        ]));
     }
 
     public function update(Request $request) {
--- routes/user/transaction.php
+++ routes/user/transaction.php
@@ -3,5 +3,8 @@
 use App\Constants\TokenConstant;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([TokenConstant::AUTH_SANCTUM, TokenConstant::AUTH_USER])->get("get", "TransactionController@get");
+Route::middleware([TokenConstant::AUTH_SANCTUM, TokenConstant::AUTH_USER])->group(function () {
+    Route::get("get", "TransactionController@get");
+    Route::get("active", "TransactionController@active");
+});
 Route::post("update", "TransactionController@update");
